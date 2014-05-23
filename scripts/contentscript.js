ccbbaa91bb17f78533d6fc922bfec8493b32bd7e
patrioticcow <cristi.catea@gmai..com>
2014-05-23 01:02:52 -0700

    0.16

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -5,6 +5,9 @@
  */
 var seek;
 var volume;
+var seekScale = 5;
+var volumeScale = 5;
+var seekState = 0;
 
 function injectJs(link) {
     $('<script type="text/javascript" src="' + link + '"/>').appendTo($('head'));
@@ -19,28 +22,26 @@ function init() {
     var playerId = document.getElementById("movie_player");
     var $playerApi = $('#player-api');
     var $playerId = $(playerId);
-    var seekState = 0;
     var pid = $playerApi.length ? $playerApi : $playerId;
 
     if (playerId) {
 
         pid.on('mousewheel', function (e) {
-
             if (seekState === 1) {
 
                 if (e.deltaY === 1) {
-                    seek = seek + 5;
+                    seek = seek + seekScale;
                 } else {
-                    seek = seek - 5;
+                    seek = seek - seekScale;
                 }
 
                 window.postMessage({type: "FROM_CONTENTSCRIPT_SEEK", key: 'seek', value: seek}, "*");
             } else {
 
                 if (e.deltaY === 1) {
-                    volume = volume >= 100 ? 100 : volume + 5;
+                    volume = volume >= 100 ? 100 : volume + volumeScale;
                 } else {
-                    volume = volume <= 0 ? 0 : volume - 5;
+                    volume = volume <= 0 ? 0 : volume - volumeScale;
                 }
 
                 window.postMessage({type: "FROM_CONTENTSCRIPT_VOLUME", key: 'volume', value: volume}, "*");
@@ -77,6 +78,26 @@ $(function () {
     if (playerIdd) {
         injectJs(chrome.extension.getURL('scripts/injected.js'));
     }
+});
+
+chrome.storage.local.get('ymc_volume', function (result) {
+    if (result.ymc_volume === true) {
+        seekState = 0;
+    }
+});
+
+chrome.storage.local.get('ymc_seek', function (result) {
+    if (result.ymc_seek === true) {
+        seekState = 1;
+    }
+});
+
+chrome.storage.local.get('ymc_jump_volume', function (result) {
+    seekScale = result.ymc_jump_volume;
+});
+
+chrome.storage.local.get('ymc_jump_seek', function (result) {
+    volumeScale = result.ymc_jump_seek;
 });
 
 /**
---
+++ scripts/options.js
@@ -0,0 +1,56 @@
+"use strict";
+
+function defaultSettings() {
+    if (this.id === 'volume_default') {
+        setDefaultSettings(true, false);
+    }
+    if (this.id === 'seek_default') {
+        setDefaultSettings(false, true);
+    }
+}
+
+function jumpSettings() {
+    var val = this.options.selectedIndex;
+    if (this.id === 'volume_jump') {
+        chrome.storage.local.set({ymc_jump_volume: val}, null);
+    }
+    if (this.id === 'seek_jump') {
+        chrome.storage.local.set({ymc_jump_seek: val}, null);
+    }
+}
+
+function setDefaultSettings(volume, seek) {
+    chrome.storage.local.set({ymc_volume: volume}, null);
+    chrome.storage.local.set({ymc_seek: seek}, null);
+}
+
+function initOptions() {
+    var volume_default = document.getElementById("volume_default");
+    var seek_default = document.getElementById("seek_default");
+    var volume_jump = document.getElementById("volume_jump");
+    var seek_jump = document.getElementById("seek_jump");
+
+    volume_default.addEventListener('click', defaultSettings);
+    seek_default.addEventListener('click', defaultSettings);
+    volume_jump.addEventListener('change', jumpSettings);
+    seek_jump.addEventListener('change', jumpSettings);
+
+    // get defaults
+    chrome.storage.local.get('ymc_volume', function (result) {
+        volume_default.checked = result.ymc_volume;
+    });
+
+    chrome.storage.local.get('ymc_seek', function (result) {
+        seek_default.checked = result.ymc_seek;
+    });
+
+    chrome.storage.local.get('ymc_jump_volume', function (result) {
+        volume_jump.options[result.ymc_jump_volume].selected  = true;
+    });
+
+    chrome.storage.local.get('ymc_jump_seek', function (result) {
+        seek_jump.options[result.ymc_jump_seek].selected  = true;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', initOptions);
