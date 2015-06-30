1ab467978134d3c52efd73a199cca809e7bc6a7a
cristi <cristi.catea@gmail.com>
2015-06-30 13:17:26 -0700

    0.9.6
    added volume only feature

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -5,87 +5,96 @@
  */
 var seek;
 var volume;
+var volumeOnly = 0;
 var seekScale;
 var volumeScale;
 var seekState = 0;
 
 function injectJs(link) {
-    $('<script type="text/javascript" src="' + link + '"/>').appendTo($('head'));
+	$('<script type="text/javascript" src="' + link + '"/>').appendTo($('head'));
 }
 
 function setVars(event) {
-    if (event.data.volume !== 'not_needed') volume = parseFloat(event.data.volume);
-    seek = parseFloat(event.data.currentTime);
+	if (event.data.volume !== 'not_needed') volume = parseFloat(event.data.volume);
+	seek = parseFloat(event.data.currentTime);
 }
 
 function init() {
-    var playerId = document.getElementById("movie_player");
-    var $playerApi = $('#player-api');
-    var $playerId = $(playerId);
-    var pid = $playerApi.length ? $playerApi : $playerId;
+	var playerId   = document.getElementById("movie_player");
+	var $playerApi = $('#player-api');
+	var $playerId  = $(playerId);
+	var pid        = $playerApi.length ? $playerApi : $playerId;
 
-    seekScale = isNaN(seekScale) ? 5 : seekScale;
-    volumeScale = isNaN(volumeScale) ? 5 : volumeScale;
+	seekScale   = isNaN(seekScale) ? 5 : seekScale;
+	volumeScale = isNaN(volumeScale) ? 5 : volumeScale;
 
-    if (playerId) {
-        pid.on('mousewheel', function (e) {
-            if (seekState === 1) {
-                if (e.deltaY === 1) {
-                    seek = seek + seekScale;
-                } else {
-                    seek = seek - seekScale;
-                }
+	if (playerId) {
+		pid.on('mousewheel', function (e) {
+			// if volume only option don't enable seek
+			if(volumeOnly === 1) seekState = 0;
 
-                window.postMessage({type: "FROM_CONTENTSCRIPT_SEEK", key: 'seek', value: seek}, "*");
-            } else {
-                if (e.deltaY === 1) {
-                    volume = volume >= 100 ? 100 : volume + volumeScale;
-                } else {
-                    volume = volume <= 0 ? 0 : volume - volumeScale;
-                }
+			if (seekState === 1) {
+				if (e.deltaY === 1) {
+					seek = seek + seekScale;
+				} else {
+					seek = seek - seekScale;
+				}
 
-                window.postMessage({type: "FROM_CONTENTSCRIPT_VOLUME", key: 'volume', value: volume}, "*");
-            }
+				window.postMessage({type: "FROM_CONTENTSCRIPT_SEEK", key: 'seek', value: seek}, "*");
+			} else {
+				if (e.deltaY === 1) {
+					volume = volume >= 100 ? 100 : volume + volumeScale;
+				} else {
+					volume = volume <= 0 ? 0 : volume - volumeScale;
+				}
 
-            e.preventDefault();
-            e.stopPropagation();
-        });
-    }
+				window.postMessage({type: "FROM_CONTENTSCRIPT_VOLUME", key: 'volume', value: volume}, "*");
+			}
 
-    $(document).mousedown(function (e) {
-        switch (e.which) {
-            case 1:
-                //left Click
-                break;
-            case 2:
-                // middle click and scroll
-                seekState = seekState === 1 ? 0 : 1;
-                break;
-            case 3:
-                //right Click
-                break;
-        }
+			e.preventDefault();
+			e.stopPropagation();
+		});
+	}
 
-        return true;
-    });
+	$(document).mousedown(function (e) {
+		switch (e.which) {
+			case 1:
+				//left Click
+				break;
+			case 2:
+				// middle click and scroll
+				seekState = seekState === 1 ? 0 : 1;
+
+				break;
+			case 3:
+				//right Click
+				break;
+		}
+
+		return true;
+	});
 }
 
 /**
  * inject the script
  */
 $(function () {
-    var playerIdd = document.getElementById("movie_player");
-    if (playerIdd) {
-        injectJs(chrome.extension.getURL('scripts/injected.js'));
-    }
+	var playerIdd = document.getElementById("movie_player");
+	if (playerIdd) {
+		injectJs(chrome.extension.getURL('scripts/injected.js'));
+	}
+});
+
+chrome.storage.local.get('ymc_volume_only', function (result) {
+	if(result.ymc_volume_only === true) volumeOnly = 1;
 });
 
 chrome.storage.local.get('ymc_volume', function (result) {
-    if (result.ymc_volume === true) seekState = 0;
+	if (result.ymc_volume === true) seekState = 0;
 });
 
 chrome.storage.local.get('ymc_seek', function (result) {
-    if (result.ymc_seek === true)  seekState = 1;
+	if (result.ymc_seek === true)  seekState = 1;
 });
 
 chrome.storage.local.get('ymc_jump_volume', function (result) {
@@ -100,15 +109,15 @@ chrome.storage.local.get('ymc_jump_seek', function (result) {
  * listen for events form the injected script
  */
 window.addEventListener("message", function (event) {
-    if (event.source != window)
-        return;
+	if (event.source != window)
+		return;
 
-    if (event.data.type && (event.data.type == "FROM_PAGE")) {
-        setVars(event);
-        init();
-    }
+	if (event.data.type && (event.data.type == "FROM_PAGE")) {
+		setVars(event);
+		init();
+	}
 
-    if (event.data.type && (event.data.type == "SEEK_FROM_PAGE")) {
-        setVars(event);
-    }
+	if (event.data.type && (event.data.type == "SEEK_FROM_PAGE")) {
+		setVars(event);
+	}
 }, false);
--- scripts/options.js
+++ scripts/options.js
@@ -1,49 +1,68 @@
 "use strict";
 
+// set defaults
 function defaultSettings() {
-    if (this.id === 'volume_default') setDefaultSettings(true, false);
-    if (this.id === 'seek_default') setDefaultSettings(false, true);
+	chrome.storage.local.get('ymc_volume_only', function () {
+		var bool = document.getElementById("volume_only").checked === true;
+		chrome.storage.local.set({ymc_volume_only: bool}, null);
+	});
+
+	chrome.storage.local.get('ymc_volume', function () {
+		var bool = document.getElementById("volume_default").checked === true;
+		chrome.storage.local.set({ymc_volume: bool}, null);
+	});
+
+	chrome.storage.local.get('ymc_seek', function () {
+		var bool = document.getElementById("seek_default").checked === true;
+		chrome.storage.local.set({ymc_seek: bool}, null);
+	});
 }
 
 function jumpSettings() {
-    var val = this.options.selectedIndex;
+	var val = this.options.selectedIndex;
 
-    if (this.id === 'volume_jump') chrome.storage.local.set({ymc_jump_volume: val}, null);
-    if (this.id === 'seek_jump') chrome.storage.local.set({ymc_jump_seek: val}, null);
-}
-
-function setDefaultSettings(volume, seek) {
-    chrome.storage.local.set({ymc_volume: volume}, null);
-    chrome.storage.local.set({ymc_seek: seek}, null);
+	if (this.id === 'volume_jump') chrome.storage.local.set({ymc_jump_volume: val}, null);
+	if (this.id === 'jump_seek') chrome.storage.local.set({ymc_jump_seek: val}, null);
 }
 
 function initOptions() {
-    var volume_default = document.getElementById("volume_default");
-    var seek_default = document.getElementById("seek_default");
-    var volume_jump = document.getElementById("volume_jump");
-    var seek_jump = document.getElementById("seek_jump");
+	var volume_only    = document.getElementById("volume_only");
+	var volume_default = document.getElementById("volume_default");
+	var seek_default   = document.getElementById("seek_default");
+	var volume_jump    = document.getElementById("volume_jump");
+	var jump_seek      = document.getElementById("jump_seek");
 
-    volume_default.addEventListener('click', defaultSettings);
-    seek_default.addEventListener('click', defaultSettings);
-    volume_jump.addEventListener('change', jumpSettings);
-    seek_jump.addEventListener('change', jumpSettings);
+	volume_only.addEventListener('click', defaultSettings);
+	volume_default.addEventListener('click', defaultSettings);
+	seek_default.addEventListener('click', defaultSettings);
+	volume_jump.addEventListener('change', jumpSettings);
+	jump_seek.addEventListener('change', jumpSettings);
 
-    // get defaults
-    chrome.storage.local.get('ymc_volume', function (result) {
-        volume_default.checked = result.ymc_volume;
-    });
+	// get defaults
+	// Use Volume Controls only
+	chrome.storage.local.get('ymc_volume_only', function (result) {
+		volume_only.checked = result.ymc_volume_only;
+	});
 
-    chrome.storage.local.get('ymc_seek', function (result) {
-        seek_default.checked = result.ymc_seek;
-    });
+	// Use Volume Controls by default
+	chrome.storage.local.get('ymc_volume', function (result) {
+		volume_default.checked = result.ymc_volume;
+	});
 
-    chrome.storage.local.get('ymc_jump_volume', function (result) {
-        volume_jump.options[result.ymc_jump_volume].selected  = true;
-    });
+	// Use Seek Controls by default
+	chrome.storage.local.get('ymc_seek', function (result) {
+		seek_default.checked = result.ymc_seek;
+	});
 
-    chrome.storage.local.get('ymc_jump_seek', function (result) {
-        seek_jump.options[result.ymc_jump_seek].selected  = true;
-    });
+	// Volume sensitivity
+	chrome.storage.local.get('ymc_jump_volume', function (result) {
+		volume_jump.options[result.ymc_jump_volume].selected = true;
+	});
+
+	// Seek sensitivity
+	chrome.storage.local.get('ymc_jump_seek', function (result) {
+		jump_seek.options[result.ymc_jump_seek].selected = true;
+	});
 }
 
 document.addEventListener('DOMContentLoaded', initOptions);
